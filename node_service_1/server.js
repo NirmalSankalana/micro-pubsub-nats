ec84c5dec3501355cbafbd966ca65f283ca6a96c
Velou User <velouuser@pop-os.localdomain>
2024-04-02 17:12:57 +0530

    Used lodash iterator

--- node_service_1/server.js
+++ node_service_1/server.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 import { StringCodec, connect } from "nats";
 
 const server = {
@@ -9,10 +10,17 @@ const main = async () => {
     const nc = await connect(server);
     console.log(`connected to ${nc.getServer()}`);
 
+    const fakeObjects = _.times(10, () => ({
+      id: _.uniqueId(),
+      name: _.sample(["Alice", "Bob", "Charlie"]),
+      age: _.random(18, 60),
+    }));
+
     const sc = StringCodec();
-    for(let i=0; i<10; i++) {
-        nc.publish("notify", sc.encode("Hello world"));
-    }   
+
+    _.forEach(fakeObjects, (o) => {
+      nc.publish("notify", sc.encode(JSON.stringify(o)));
+    });  
 
   } catch (err) {
     console.log(`error connecting to ${JSON.stringify(server)}`);
